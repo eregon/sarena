1a0a3f998bbbd42e4ef5f56e51d176e05cd5a965
Xavier <xaxa-@hotmail.com>
2012-11-03 04:06:47 +0100

    questions 3.2 mesures et comparaison minimax alphabeta

--- basic_player.py
+++ basic_player.py
@@ -7,6 +7,7 @@ import random
 
 from sarena import *
 import minimax
+import time
 
 RED = -1
 YELLOW = 1
@@ -19,16 +20,17 @@ class AlphaBetaPlayer(Player, minimax.Game):
     the board.
 
     """
-
+    nodes_amount=0
     def successors(self, board):
         for action in board.get_actions():
             new_board = board.clone()
             new_board.play_action(action)
+            AlphaBetaPlayer.nodes_amount+=1
             yield action, new_board
 
     def cutoff(self, board, depth):
         # TODO: remove depth limitation
-        return depth >= 1 or board.is_finished()
+        return depth >= 3 or board.is_finished()
 
     def evaluate(self, board):
         score = board.get_score()
@@ -45,8 +47,20 @@ class AlphaBetaPlayer(Player, minimax.Game):
     def play(self, percepts, step, time_left):
         self.player = RED if step % 2 == 0 else YELLOW
         board = Board(percepts, invert=(self.player==YELLOW))
-        return minimax.search(board, self)
-
+        nodes_before = AlphaBetaPlayer.nodes_amount      
+        t1 = time.clock() 
+        '''replace clock by time on unix systems'''
+        '''result = minimax.search(board, self)   '''
+        result = minimax.search(board, self, False)          
+        t2 = time.clock() 
+        '''replace clock by time on unix systems'''
+        print("Amount of nodes visited:", AlphaBetaPlayer.nodes_amount-nodes_before);
+        print("Time required:", t2-t1)
+        print("Player : ", self.player)
+        print ("Result : ", result)
+        return result;
 
 if __name__ == "__main__":
     player_main(AlphaBetaPlayer())
+    print("Total nodes :", AlphaBetaPlayer.nodes_amount)
+    
