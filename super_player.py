de4540331231696ccb51755afcd615881e0fac97
Benoit Daloze <eregontp@gmail.com>
2012-11-08 08:42:16 +0100

    add a minimal player as a base for our bad-ass player

---
+++ super_player.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''Benoit Daloze & Xavier de Ryckel'''
+
+import random
+
+from sarena import *
+import minimax
+
+# We are always the yellow player
+class SuperPlayer(Player, minimax.Game):
+    def successors(self, board):
+        for action in board.get_actions():
+            new_board = board.clone()
+            new_board.play_action(action)
+            yield action, new_board
+
+    def cutoff(self, board, depth):
+        # TODO: remove depth limitation
+        return depth >= 3 or board.is_finished()
+
+    def evaluate(self, board):
+        return board.get_score()
+
+    def play(self, percepts, step, time_left):
+        board = Board(percepts)
+        return minimax.search(board, self)
+
+if __name__ == "__main__":
+    player_main(SuperPlayer())
