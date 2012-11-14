8c2c7f0a5f170727cb89d39b2f8b61071c623e73
Benoit Daloze <eregontp@gmail.com>
2012-11-14 11:07:06 +0100

    adapt tests

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
-from super_player import *
+SUPER = 1
+EVAL_PLAYER = 2
+
+MODEL=EVAL_PLAYER # SUPER
+SUPER_MODEL = MODEL == SUPER
+
+if SUPER_MODEL:
+    from super_player import *
+else:
+    from eval_player_ours import *
+    class State:
+        def setup():
+            pass
+        def score(state):
+            return EvalPlayerOurs.evaluate(None, state)
 
 import unittest
 
@@ -8,7 +22,7 @@ class TestEvaluation(unittest.TestCase):
 
     def code2color(self, code):
         if code == " ":
-            return 42
+            return 0
         elif code == "Y":
             return 1
         elif code == "R":
@@ -17,16 +31,34 @@ class TestEvaluation(unittest.TestCase):
             return 0
 
     def parse(self, state):
-        s = list(range(36))
+        if SUPER_MODEL:
+            s = list(range(36))
+        else:
+            s = Board(random_board())
+            m = s.m
         for i, line in enumerate(state.strip().splitlines()):
             for j, c in enumerate(line.split()):
                 h = int(c[0])
                 if h:
                     bot = self.code2color(c[1])
                     top = self.code2color(c[2])
-                    s[i*6+j] = (h, bot, top)
+                    if SUPER_MODEL:
+                        s[i*6+j] = (h, bot, top)
+                    else:
+                        if h == 1:
+                            m[i][j] = [m[i][j][0], [bot, top], [0, 0], [0, 0], [0, 0]]
+                        elif h == 2:
+                            m[i][j] = [m[i][j][0], [bot, 2], [2, top], [0, 0], [0, 0]]
+                        elif h == 3:
+                            m[i][j] = [m[i][j][0], [bot, 2], [2, 2], [2, top], [0, 0]]
+                        elif h == 4:
+                            m[i][j] = [m[i][j][0], [bot, 2], [2, 2], [2, 2], [2, top]]
                 else:
-                    s[i*6+j] = EMPTY_PILE
+                    if SUPER_MODEL:
+                        s[i*6+j] = EMPTY_PILE
+                    else:
+                        m[i][j] = [m[i][j][0], [0, 0], [0, 0], [0, 0], [0, 0]]
+
         return s
 
     def test_eval(self):
@@ -115,7 +147,7 @@ class TestEvaluation(unittest.TestCase):
           + 0 - 1*SURE_THING + 0 \
           - 2*SURE_THING \
           - 4*SURE_THING \
-          + 4*SURE_THING - MAYBE - LOST
+          + 4*SURE_THING - MAYBE
         self.assertEqual(State.score(self.parse(state)), s)
 
 if __name__ == '__main__':
